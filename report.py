a338c8523330cc978b492d843d46730d591ecc3a
Gary <25782129+gazza-75@users.noreply.github.com>
2021-02-04 15:41:51 +0000

    Update report.py

--- report.py
+++ report.py
@@ -3,4 +3,6 @@
 # What does it do?
 
 # * Reports URL to phishtank
-# * sends email to ASN
+# * sends email to abuse contact at ASN as per RIPE dataset
+# * Issue Domain takedown to domain registrar
+# * Advise user to forward to report@phishing.gov.uk
